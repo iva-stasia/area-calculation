35407d4ca9eb7192de9b3e68a6296c58d62f3703
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-03-06 22:35:31 +0200

    Create separate functions for each shape

--- domOperations.js
+++ domOperations.js
@@ -1,36 +1,41 @@
 function showResultInDomKvadrat() {
-    const sideLength = document.querySelector('#dlinaStoroniKvadrata').value;
+    const squareSide = document.querySelector('#dlinaStoroniKvadrata').value;
     const resultField = document.querySelector('#ploschadKvadrata');
 
-    resultField.innerHTML = calculateArea(sideLength, sideLength);
+    resultField.innerHTML = calculateSquareArea(squareSide);
 }
 
 function showResultInDomPriamougolnika() {
-    const sideALength = document.querySelector(
+    const rectSideA = document.querySelector(
         '#pervayaStoronaPriamougolnika'
     ).value;
-    const sideBLength = document.querySelector(
+    const rectSideB = document.querySelector(
         '#drugayaStoronaPriamougolnika'
     ).value;
     const resultField = document.querySelector('#ploschadPriamougolnika');
 
-    resultField.innerHTML = calculateArea(sideALength, sideBLength);
+    resultField.innerHTML = calculateRectangleArea(rectSideA, rectSideB);
 }
 
 function showResultInDomParalelograma() {
-    const sideLength = document.querySelector(
+    const parallelogramSide = document.querySelector(
         '#dlinaStoronyParalelograma'
     ).value;
-    const hLength = document.querySelector('#dlinaVisotyParalelograma').value;
+    const parallelogramHeight = document.querySelector(
+        '#dlinaVisotyParalelograma'
+    ).value;
     const resultField = document.querySelector('#ploschadParalelograma');
 
-    resultField.innerHTML = calculateArea(sideLength, hLength);
+    resultField.innerHTML = calculateParallelogramArea(
+        parallelogramSide,
+        parallelogramHeight
+    );
 }
 
 function showResultInDomRomba() {
-    const sideLength = document.querySelector('#dlinaStoronyRomba').value;
-    const hLength = document.querySelector('#dlinaVisotyRomba').value;
+    const rhombusSide = document.querySelector('#dlinaStoronyRomba').value;
+    const rhombusHeight = document.querySelector('#dlinaVisotyRomba').value;
     const resultField = document.querySelector('#ploschadRomba');
 
-    resultField.innerHTML = calculateArea(sideLength, hLength);
+    resultField.innerHTML = calculateRhombusArea(rhombusSide, rhombusHeight);
 }
--- libraryFormula.js
+++ libraryFormula.js
@@ -1,3 +1,24 @@
-function calculateArea(sideLength1, sideLength2) {
-    return sideLength1 * sideLength2;
+function multiplyPositiveTwoNumbers(firstNumber, secondNumber) {
+    if (firstNumber > 0 && secondNumber > 0) {
+        return firstNumber * secondNumber;
+    } else {
+        alert('Значения параметров должны быть больше 0.');
+        return '';
+    }
+}
+
+function calculateSquareArea(squareSide) {
+    return multiplyPositiveTwoNumbers(squareSide, squareSide);
+}
+
+function calculateRectangleArea(rectSideA, rectSideB) {
+    return multiplyPositiveTwoNumbers(rectSideA, rectSideB);
+}
+
+function calculateParallelogramArea(parallelogramSide, parallelogramHeight) {
+    return multiplyPositiveTwoNumbers(parallelogramSide, parallelogramHeight);
+}
+
+function calculateRhombusArea(rhombusSide, rhombusHeight) {
+    return multiplyPositiveTwoNumbers(rhombusSide, rhombusHeight);
 }
